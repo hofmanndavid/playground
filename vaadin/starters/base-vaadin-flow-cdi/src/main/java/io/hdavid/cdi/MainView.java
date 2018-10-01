83e91c7bcd3d03d97b780c2a66e71ee1b55c8662
David Hofmann <hofmanndavid@gmail.com>
2018-10-01 14:41:14 -0400

    vaadin starters aded

---
+++ vaadin/starters/base-vaadin-flow-cdi/src/main/java/io/hdavid/cdi/MainView.java
@@ -0,0 +1,25 @@
+package io.hdavid.cdi;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+import javax.inject.Inject;
+
+/**
+ * The main view contains a simple label element and a template element.
+ */
+@Route("")
+public class MainView extends VerticalLayout {
+
+    @Inject
+    private MessageBean messageBean;
+
+    public MainView() {
+        Button button = new Button("Click me",
+                event -> Notification.show(messageBean.getMessage()));
+        add(button);
+    }
+
+}
